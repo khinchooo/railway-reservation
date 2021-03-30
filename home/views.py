ce15fbf1729ffe8af8b3bf6bcff75b52d3d0b6f3
khinchooo <khinchooo@mm.cybermissions.co.jp>
2021-03-30 20:03:58 +0900

    registerとhomeページ画面の機能を追加する。

--- home/views.py
+++ home/views.py
@@ -1,12 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.template import loader
 from django.http import HttpResponse
+from .forms import UserLogin, UserRegister
+from django.contrib.auth import authenticate,login
+from booking.models import Member
 
 # Create your views here.
 def Login(request):
     template = loader.get_template('booking/login.html')
-    return HttpResponse(template.render({}, request))
+    if request.method == 'POST':
+        form = UserLogin(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            username = data['username']
+            password = data['password']
+            user = authenticate(username = username, password = password)
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return redirect('home/', request)
+            return HttpResponse('Invalid username or password')
+        return HttpResponse('Not Complete Data')
+    else:
+        return HttpResponse(template.render({}, request))
 
 def Register(request):
     template = loader.get_template('booking/register.html')
-    return HttpResponse(template.render({}, request))
+    if request.method == 'POST':
+        form = UserRegister(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            data = form.cleaned_data
+            username = data['username']
+            password = data['password']
+            email = data['email']
+            number = data['number']
+            member = Member()
+            member.username = username
+            member.password = password
+            member.email = email
+            member.number = number
+            member.save()
+            user.set_password(password)
+            user.save()
+            user= authenticate(username = username, password = password)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return render(request,'booking/home.html')
+            return HttpResponse('valid')
+        return HttpResponse(template.render({'form':form},request))
+    else:
+        return HttpResponse(template.render({}, request))
+
+def home(request):
+    return render(request,'booking/home.html')
--- rail/urls.py
+++ rail/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.Login,name='log'),
     path('register/',views.Register,name="reg"),
+    path('home/',views.home,name="home"),
 ]
